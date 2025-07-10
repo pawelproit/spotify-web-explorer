5da0159fae42e4ddb0ec4cc700d57e10569276b1
Paweł Pronobis <pronobispawe@gmail.com>
2025-07-10 12:48:09 +0200

    Pobieranie tokenu,wywołanie callback po logowaniu

--- art-explorer-spotify/src/app/app.routes.ts
+++ art-explorer-spotify/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Callback } from './callback/callback';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path:'callback', component:Callback}
+];
--- art-explorer-spotify/src/app/app.ts
+++ art-explorer-spotify/src/app/app.ts
@@ -1,5 +1,31 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpParams } from '@angular/common/http';
+
+
+function randomString(lenght:number){
+  const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var result = '';
+  for (var i = lenght; i > 0 ; i--){
+    result += chars[Math.floor(Math.random() * chars.length)];
+  }
+
+  return result
+}
+
+async function generateCodeChallenge(codeVerifier: string): Promise<string> {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(codeVerifier);
+  const digest = await window.crypto.subtle.digest('SHA-256', data);
+
+  let hashArray = Array.from(new Uint8Array(digest));
+  let hashString = String.fromCharCode(...hashArray);
+  let base64 = btoa(hashString)
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=+$/, '');
+  return base64;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,8 +33,36 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
+
 export class App {
   protected readonly title = signal('art-explorer-spotify');
+
+  async onAuthClick() {
+    const generated_state: string = randomString(16);
+    const codeVerifier: string = randomString(128);
+
+    const codeChallenge = await generateCodeChallenge(codeVerifier);
+
+
+    localStorage.setItem('auth-state',generated_state);
+    localStorage.setItem('code-verifier', codeVerifier)
+
+    const baseURL:string = 'https://accounts.spotify.com/en/authorize?';
+
+    const paramObject = {
+      response_type: 'code',
+      client_id: '6fa458202a164dcc917553462cc12ff1',
+      scope: 'user-read-private user-read-email',
+      redirect_uri: 'http://127.0.0.1:53661/callback',
+      state: generated_state,
+      code_challenge: codeChallenge,
+      code_challenge_method: 'S256'
+    }
+
+    const paramString = new HttpParams({fromObject:paramObject}).toString();
+    const authLink = baseURL + paramString;
+    window.location.href = authLink;
+  }
 
   //test
 }
