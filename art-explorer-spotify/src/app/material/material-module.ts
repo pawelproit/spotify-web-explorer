103cd90f4ccc703bb3443a35c32becb6d40c2127
Paweł Pronobis <pronobispawe@gmail.com>
2025-07-21 10:40:56 +0200

    Poprawa responsywności w home

--- art-explorer-spotify/src/app/material/material-module.ts
+++ art-explorer-spotify/src/app/material/material-module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 const MaterialComponents = [
   MatCardModule,
   MatDividerModule,
-  MatButtonModule
-
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatListModule,
+  MatIconModule
 ]
 
 @NgModule({
